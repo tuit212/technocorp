d438e8fe0c9c2ec7356fbb70d88cc0b0f06d47cc
tuit212 <asadbekanvarov9808@example.com>
2023-02-20 22:53:15 +0500

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Footer.scss";
+import FooterBacground from '../../Assets/Img/Footer/background.png';
+import FooterLogo from '../../Assets/Img/Footer/footer-logo.svg';
+
+
+
+function Footer() {
+  return (
+    <footer className='footer' style={{ backgroundImage: `url(${FooterBacground})` }}>
+      <div className='container'>
+        <div className='footer__container'>
+            <img src={FooterLogo} alt=""/>
+        </div>
+      </div>
+      <span className='footer__line'></span>
+      <div className='container'>
+        <p className='footer__text'>©2023. Barcha hizmatlar litsenziyalangan</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div className='container'>
+      salom
+    </div>
+  )
+}
+
+export default Header
